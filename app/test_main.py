31d36b012790ce40de1155842cee662a1e1ee937
GabrielF <gferrazsiqueira05@gmail.com>
2026-06-14 11:53:46 -0300

    result

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,17 @@
-# write your code here
+from app.main import check_password
+import pytest
+
+
+@pytest.fixture
+def check_password_fixture() -> None:
+    return check_password
+
+
+def test_password_rules(check_password_fixture: str) -> None:
+    assert check_password_fixture("Pass@word1") is True
+    assert check_password_fixture("qwerty") is False
+    assert check_password_fixture("Str@ng") is False
+    assert check_password_fixture("VeryLongPassword123@") is False
+    assert check_password_fixture("Password@") is False
+    assert check_password_fixture("Password1") is False
+    assert check_password_fixture("password1@") is False
